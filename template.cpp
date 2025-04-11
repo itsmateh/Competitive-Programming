08086b7d79c83a3b7fd793ce74224f851acc79de
itsmateh <87345589+Matewh@users.noreply.github.com>
2025-04-10 23:03:27 -0300

    Template

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using vb = vector<bool>; 
+using vl = vector<ll>;
+using vi = vector<int>;
+using vll = vector<vector<ll>>;
+using pii = pair<int,int>;
+const ll MOD = 1e9+7;
+const int INF = 1<<30; 
+const int MAXN  = 2e5+5;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define FIN ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define dbg(x) cerr<<#x<<" = "<<(x)<<endl;
+#define raya cerr << "=================" << endl;
+#define forn(i,n) for(ll i=0;i<ll(n);i++)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define dforn(i,n) for(ll i=ll(n)-1;i>=0;i--)
+#define pb push_back
+#define mp make_pair
+#define endl "\n";
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define esta(x, c) ((c).find(x) != (c).end())
+
+
+int main() {
+    FIN;
+    
+    
+    return 0;
+}
